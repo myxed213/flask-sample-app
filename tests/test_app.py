fb82ff21d32e614cfd26058b7950bf06f9cb9df6
Peter Joshua P. Pagunsan <202180231@psu.palawan.edu.ph>
2025-06-03 14:15:51 +0800

    Fix test cases to match updated API response structure

--- tests/test_app.py
+++ tests/test_app.py
@@ -16,19 +16,19 @@ class TestAppRoutes(unittest.TestCase):
     def test_hello_route(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), "Hello, Flask!")
+        self.assertIn("<title>Flask CRUD Demo</title>", response.data.decode('utf-8'))
 
     def test_add_item_route(self):
         response = self.client.post('/items', json={"name": "item1"})
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.get_json()['message'], 'Item added successfully')
-        self.assertEqual(response.get_json()['item'], {'name': 'item1'})
+        self.assertEqual(response.get_json()['item'], {'id': 0, 'name': 'item1', 'price': 0.0})
 
     def test_get_item_route(self):
         self.client.post('/items', json={"name": "item1"})
         response = self.client.get('/items/0')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get_json(), {'item': {'name': 'item1'}})
+        self.assertEqual(response.get_json(), {'item': {'id': 0, 'name': 'item1', 'price': 0.0}})
 
     def test_get_nonexistent_item_route(self):
         response = self.client.get('/items/99')
@@ -39,7 +39,7 @@ class TestAppRoutes(unittest.TestCase):
         self.client.post('/items', json={"name": "old name"})
         response = self.client.put('/items/0', json={"name": "new name"})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get_json()['item'], {"name": "new name"})
+        self.assertEqual(response.get_json()['item'], {"id": 0, "name": "new name", "price": 0.0})
 
     def test_update_nonexistent_item_route(self):
         response = self.client.put('/items/5', json={"name": "fail"})
@@ -51,7 +51,7 @@ class TestAppRoutes(unittest.TestCase):
         response = self.client.delete('/items/0')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get_json()['message'], 'Item deleted successfully')
-        self.assertEqual(response.get_json()['item'], {'name': 'to be deleted'})
+        self.assertEqual(response.get_json()['item'], {'id': 0, 'name': 'to be deleted', 'price': 0.0})
 
     def test_delete_nonexistent_item_route(self):
         response = self.client.delete('/items/100')
